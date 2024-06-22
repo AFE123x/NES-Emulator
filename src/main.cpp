e6aae5da541acf7df44c774f0212062e7a8f973d
AFE123x <arunf400@gmail.com>
2024-06-21 22:55:06 -0400

    remove branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,5 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    int fd = open("../tests/assembly/6502_functional_test.bin",O_RDONLY);
-    BUS bus;
-    char buf = 0;
-    read(fd,&buf,1);
-    for(int i = 0; i <= 0xFFFF; i++){
-        std::cout<<"address: "<<i<<std::endl;
-        bus.cpuwrite(i,buf);
-        read(fd,&buf,1);
-    }
-    bus.execute();
-    return 0;
+
 }
