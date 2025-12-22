69cdf533f3944089be02afb71923cb500d9bf028
AFE123x <arunf400@gmail.com>
2025-12-22 00:17:30 -0800

    Added slog for logging system
    - Based on benchmarks, slog provides the best performance.
    - added slog, and various libraries to cargo.toml, and cargo.lock
    - added example usage to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use slog::{Drain, Logger};
+
 fn main() {
-    println!("Hello, world!");
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator)
+        .build()
+        .fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = Logger::root(drain, slog::o!());
+    slog::info!(log, "hello world");
 }
